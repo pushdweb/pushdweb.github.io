2019a6f849da23ded514d7a82333b20d03b7f699
Kieran Fraser <kfraser@tcd.ie>
2019-11-23 10:13:44 +0000

    Updated with push

---
+++ assets/js/sw.js
@@ -0,0 +1,21 @@
+self.addEventListener('push', function(event) {
+  console.log('[Service Worker] Push Received.');
+  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+
+  const title = 'Push Codelab';
+  const options = {
+    body: event.data.text()
+  };
+
+  event.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener('notificationclick', function(event) {
+  console.log('[Service Worker] Notification click Received.');
+
+  event.notification.close();
+
+  event.waitUntil(
+    clients.openWindow('https://fraserkieran.com')
+  );
+});
--- assets/js/test.js
+++ assets/js/test.js
@@ -1,10 +1,9 @@
-// Set up Firebase instance
-
-// Request permission from user
-
-// Create 
+// Useful - https://developers.google.com/web/fundamentals/codelabs/push-notifications
 
 var db = null;
+var swRegistration = null;
+var isSubscribed = false;
+const applicationServerPublicKey = 'BJEmLHcgkIMhmtM1RvtUtpg01ue_ZJUrWxY42_IlR5KgNMjKHH8DT9bM4xP8w9CJOJpyf2_dVpORdS99vPoFnSQ';
 
 $.getScript('https://www.gstatic.com/firebasejs/7.4.0/firebase-app.js', function()
 {
@@ -23,20 +22,140 @@ $.getScript('https://www.gstatic.com/firebasejs/7.4.0/firebase-app.js', function
         };
         // Initialize Firebase
         var empushyApp = firebase.initializeApp(firebaseConfig, "empushyApp");
-
         db = empushyApp.database();
+        
+        /* Check if service workers and push messaging is supported by the browser */
+        if ('serviceWorker' in navigator && 'PushManager' in window) {
+          console.log('Service Worker and Push is supported');
 
-        navigator.serviceWorker.register('assets/js/sw.js').then( function(result){
-            
-            displayNotification();
-        });
+          navigator.serviceWorker.register('assets/js/sw.js')
+          .then(function(swReg) {
+            console.log('Service Worker is registered', swReg);
 
-        Notification.requestPermission (status => {
-            console.log('Notification permission status: ', status)
-            //displayNotification();
-        })
+            swRegistration = swReg;
+            initializeUI();
+              
+          })
+          .catch(function(error) {
+            console.error('Service Worker Error', error);
+          });
+        } else {
+          console.warn('Push messaging is not supported');
+        }
+        
     });
 });
+
+/* Set button text depending if user subbed or not */
+function initializeUI() {
+  // Set the initial subscription value
+  swRegistration.pushManager.getSubscription()
+  .then(function(subscription) {
+    isSubscribed = !(subscription === null);
+
+    if (isSubscribed) {
+      console.log('User IS subscribed.');
+    } else {
+      console.log('User is NOT subscribed.');
+    }
+
+    updateBtn();
+  });
+}
+
+function updateBtn() {
+
+  if (Notification.permission === 'denied') {
+    $('#subButton').prop('disabled', true);
+    updateSubscriptionOnServer(null);
+    return;
+  }
+    
+  if (isSubscribed) {
+      $('#subButton').html('Unsubscribe');
+  } else {
+      $('#subButton').html('Subscribe');
+  }
+
+  $('#subButton').prop('disabled', false);
+}
+
+function subButtonClick(){
+    console.log('subscription button clicked')
+    $('#subButton').prop('disabled', true);
+    
+    if (isSubscribed) {
+      unsubscribeUser();
+    } else {
+      subscribeUser();
+    }
+}
+
+function unsubscribeUser() {
+  swRegistration.pushManager.getSubscription()
+  .then(function(subscription) {
+    if (subscription) {
+      return subscription.unsubscribe();
+    }
+  })
+  .catch(function(error) {
+    console.log('Error unsubscribing', error);
+  })
+  .then(function() {
+    updateSubscriptionOnServer(null);
+
+    console.log('User is unsubscribed.');
+    isSubscribed = false;
+
+    updateBtn();
+  });
+}
+
+function subscribeUser() {
+  const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey);
+  swRegistration.pushManager.subscribe({
+    userVisibleOnly: true,
+    applicationServerKey: applicationServerKey
+  })
+  .then(function(subscription) {
+    console.log('User is subscribed.');
+
+    updateSubscriptionOnServer(subscription);
+
+    isSubscribed = true;
+
+    updateBtn();
+  })
+  .catch(function(err) {
+    console.log('Failed to subscribe the user: ', err);
+    updateBtn();
+  });
+}
+
+function updateSubscriptionOnServer(subscription) {
+  // TODO: Send subscription to application server
+
+  if (subscription) {
+    console.log(JSON.stringify(subscription));
+  } else {
+    console.log('subscription was null')
+  }
+}
+
+function urlB64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - base64String.length % 4) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, '+')
+    .replace(/_/g, '/');
+
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
 
 function writeUserData(userId, name, email) {
     if(db!=null){
