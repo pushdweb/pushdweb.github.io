17e0965ec095f961874ef9f4bf287cb14075e393
Kieran Fraser <kfraser@tcd.ie>
2019-12-15 12:30:16 +0000

    Testing worker

--- sw.js
+++ sw.js
@@ -43,7 +43,7 @@ self.addEventListener('notificationclick', function(event) {
         // Was a normal notification click
         console.log('Notification Click.');
         update_engagement(event, 'clicked')
-        /*event.waitUntil(
+        event.waitUntil(
             clients.matchAll({includeUncontrolled: true, type: 'window'}).then(function(clients) {
                 for (i = 0; i < clients.length; i++) {
                     console.log(client[i])
@@ -51,7 +51,7 @@ self.addEventListener('notificationclick', function(event) {
                     clients[i].focus();
                 }
             })
-        );*/
+        );
         return;
     }
 
